2902aa2f7aad584b8a572040c57b385076ba7e92
⚡ŞŦΔŘŁØŘĐ <soumodip.dgp@gmail.com>
2025-08-06 12:05:15 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -1,93 +1,56 @@
-#
-# Copyright (C) 2022-2023 The LineageOS Project
+# Copyright 2014 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# A/B
-TARGET_IS_VAB := true
-
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2400
-TARGET_SCREEN_WIDTH := 1080
-
-# Screen
-TARGET_SCREEN_DENSITY := 420
+# Device path
+DEVICE_PATH := device/motorola/rhodei/rootdir
 
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
-PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-PRODUCT_SHIPPING_API_LEVEL := 31
+PRODUCT_PROPERTY_OVERRIDES := \
+    ro.sf.lcd_density=420
 
-# Inherit from motorola sm6375-common
-$(call inherit-product, device/motorola/sm6375-common/common.mk)
+# Brightness
+SOONG_CONFIG_qtidisplay_brightness := true
 
-# Overlays
-PRODUCT_PACKAGES += \
-    FrameworksResRhodei \
-    LineageSystemUIRhodei \
-    SettingsProviderResRhodei \
-    SystemUIResRhodei \
-    WifiResRhodei
-
-# Audio
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/audio/audio_io_policy.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_io_policy.conf \
-    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
-    $(LOCAL_PATH)/audio/mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths.xml \
-    $(LOCAL_PATH)/audio/audio_policy_volumes.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_volumes.xml \
-    $(LOCAL_PATH)/audio/audio_effects.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/audio/audio_ext_spkr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/audio_ext_spkr.conf \
-    $(LOCAL_PATH)/audio/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_policy_configuration.xml \
-    $(LOCAL_PATH)/audio/default_volume_tables.xml:$(TARGET_COPY_OUT_VENDOR)/etc/default_volume_tables.xml
+# Device characteristics
+DEVICE_CHARACTERISTICS += hfr
 
 # Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.rhodei \
-    com.motorola.hardware.biometric.fingerprint@1.0.vendor
+TARGET_USES_EGISTEC_FINGERPRINT := true
+TARGET_USES_FOCAL_FINGERPRINT := true
 
-$(foreach f,$(wildcard $(LOCAL_PATH)/configs/idc/*.idc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/usr/idc/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/configs/keylayout/*.kl),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/$(notdir $f)))
+# Model
+PRODUCT_MODEL := moto g(62)
 
-# Init
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/hw/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/hw/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/etc/init/*.rc),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/etc/init/$(notdir $f)))
-$(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
-        $(eval PRODUCT_COPY_FILES += $(f):$(TARGET_COPY_OUT_VENDOR)/bin/$(notdir $f)))
+# Modules
+BOOT_KERNEL_MODULES := \
+    sm5602_fg_mmi.ko \
+    sgm4154x_charger.ko \
+    bq2589x_charger.ko \
+    tcpc_sgm7220.ko \
+    rt_pd_manager.ko \
+    ili9882_mmi.ko \
+    nova_0flash_mmi.ko
 
-# Lineage Touch
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.rhodei
+# Power
+TARGET_IS_BLAIR := true
 
-# Permissions
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
-    frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
-    frameworks/native/data/etc/android.hardware.nfc.uicc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.uicc.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/com.nxp.mifare.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/com.nxp.mifare.xml
+# Inherit from those products. Most specific first.
+$(call inherit-product, device/motorola/sm4350-common/platform.mk)
 
-# Sensors
-PRODUCT_PACKAGES += \
-    sensors.rhodei
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
-# Soong namespaces
-PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
-
-# Thermal
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/thermal-engine-rhodei.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-rhodei.conf
-
-# Inherit from vendor blobs
-$(call inherit-product, vendor/motorola/rhodei/rhodei-vendor.mk)
+# include board vendor blobs
+$(call inherit-product-if-exists, vendor/motorola/rhodei/rhodei-vendor.mk)
