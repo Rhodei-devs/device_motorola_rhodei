9f31a37ac4faf06470a376aff192a6e9a95205de
⚡ŞŦΔŘŁØŘĐ <soumodip.dgp@gmail.com>
2025-08-06 11:00:50 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,72 +1,26 @@
-# Copyright 2014 The Android Open Source Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# Copyright (C) 2022-2023 The LineageOS Project
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# SPDX-License-Identifier: Apache-2.0
 #
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 
-# Platform
-PRODUCT_PLATFORM := holi
-include device/motorola/sm6375-common/common.mk
+DEVICE_PATH := device/motorola/rhodei
 
-# Bootloader Name
+# Inherit from motorola sm6375-common
+include device/motorola/sm6375-common/BoardConfigCommon.mk
+
+# Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := rhodei
 
-# Modules
-BOARD_VENDOR_KERNEL_MODULES_LOAD := \
-    sm5602_fg_mmi.ko \
-    sgm4154x_charger.ko \
-    bq2589x_charger.ko \
-    tcpc_sgm7220.ko \
-    adsp_loader_dlkm.ko \
-    apr_dlkm.ko \
-    aw882xx_acf.ko \
-    bolero_cdc_dlkm.ko \
-    camera.ko \
-    ldo_vibrator_mmi.ko \
-    machine_dlkm.ko \
-    mbhc_dlkm.ko \
-    mmi_sys_temp.ko \
-    native_dlkm.ko \
-    sec_nfc.ko \
-    pinctrl_lpi_dlkm.ko \
-    platform_dlkm.ko \
-    q6_dlkm.ko \
-    q6_notifier_dlkm.ko \
-    q6_pdr_dlkm.ko \
-    qca_cld3_wlan.ko \
-    ili9882_mmi.ko \
-    nova_0flash_mmi.ko \
-    rx_macro_dlkm.ko \
-    snd_event_dlkm.ko \
-    stub_dlkm.ko \
-    swr_ctrl_dlkm.ko \
-    swr_dlkm.ko \
-    tx_macro_dlkm.ko \
-    va_macro_dlkm.ko \
-    wcd937x_dlkm.ko \
-    wcd937x_slave_dlkm.ko \
-    wcd938x_dlkm.ko \
-    wcd938x_slave_dlkm.ko \
-    wcd9xxx_dlkm.ko \
-    wcd_core_dlkm.ko \
-    wlan.ko \
-    wsa881x_analog_dlkm.ko \
-    rt_pd_manager.ko
+# Kernel
+BOARD_KERNEL_CMDLINE += androidboot.hab.product=rhodei
+TARGET_KERNEL_CONFIG += vendor/rhodei_defconfig
 
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := \
-    sm5602_fg_mmi.ko \
-    sgm4154x_charger.ko \
-    bq2589x_charger.ko \
-    tcpc_sgm7220.ko \
-    rt_pd_manager.ko
+# Kernel Modules
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load))
+BOARD_VENDOR_KERNEL_MODULES_BLOCKLIST_FILE := $(DEVICE_PATH)/modules.blocklist
+BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD := $(strip $(shell cat $(DEVICE_PATH)/modules.load.recovery))
+BOOT_KERNEL_MODULES := $(BOARD_VENDOR_RAMDISK_RECOVERY_KERNEL_MODULES_LOAD)
 
 # Partition information
 BOARD_SUPER_PARTITION_SIZE := 8613003264
